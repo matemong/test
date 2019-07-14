481f8c6c44c0546166bb3bc4873542602da4c3d0
Máté Mong <mongmate811@gmail.com>
2019-07-14 09:58:12 +0200

    just playing around

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Mekk Elek',
+    username: 'Elek',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Mülner Krisztofer',
+    username: 'Antonette',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Szepesi Szalók',
+    username: 'Szalok',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Pethe Zádor',
+    username: 'Zador',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Forgács Zorán',
+    username: 'Zoran',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Fehér Attila',
+    username: 'Attila',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Gyarmathi Firenze',
+    username: 'Firenze',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Szölösi Kriszta',
+    username: 'Kriszta',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Nemes Vayk',
+    username: 'Vayk',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Vida Orsolya',
+    username: 'Orsolya',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
